8d5b6dff1238286065d240a670d32af717a12b24
hongxiao95 <hongxiao95@hotmail.com>
2019-02-01 00:59:28 +0800

    init 2

---
+++ src/main/java/org/hongxiao/HuaRongDao.java
@@ -0,0 +1,13 @@
+package org.hongxiao;
+
+/**
+ * Hello world!
+ *
+ */
+public class HuaRongDao
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
